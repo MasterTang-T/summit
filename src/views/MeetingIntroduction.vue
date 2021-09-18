950faecafbfd12c58310dd74b0951f51e45197b7
Tang_tom <1286545749@qq.com>
2021-09-18 16:01:49 +0800

    introduction

--- src/views/MeetingIntroduction.vue
+++ src/views/MeetingIntroduction.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="MeetingIntroduction">
+    <Header />
     <img src="../assets/MeetingIntroduction.jpg" alt="" />
   </div>
 </template>
 <script lang='ts'>
 import { defineComponent } from "vue";
+import Header from "components/Header.vue";
+
 export default defineComponent({
   name: "MeetingIntroduction",
+  components: {
+    Header,
+  },
   setup() {},
 });
 </script>
